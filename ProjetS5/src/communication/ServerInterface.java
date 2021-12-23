7dbc464caa88e68c377a7d52f74820c2463bb0d1
defre <defreitas.tony.pro@gmail.com>
2021-12-23 17:42:30 +0100

    Modifications ...

--- ProjetS5/src/communication/ServerInterface.java
+++ ProjetS5/src/communication/ServerInterface.java
@@ -1,6 +1,6 @@
 package communication;
 
-import Client.User;
+import client.User;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -8,7 +8,7 @@ import java.rmi.RemoteException;
 public interface ServerInterface extends Remote {
     void register(User user) throws RemoteException;
     void unregister(User user) throws RemoteException;
-    void sendMessage(String text, int idThread) throws RemoteException;
+    void sendMessage(User sender, String text, int idThread) throws RemoteException;
     void hasReceived(User user, int idMessage) throws RemoteException;
     void hasRead(User user, int idMessage) throws RemoteException;
     void pong() throws RemoteException;
