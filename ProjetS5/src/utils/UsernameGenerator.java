07970b1e9c0784a6bcb5250a8e1caa47fbb90e20
defre <defreitas.tony.pro@gmail.com>
2022-01-07 18:29:02 +0100

    some changes

--- ProjetS5/src/utils/UsernameGenerator.java
+++ ProjetS5/src/utils/UsernameGenerator.java
@@ -1,6 +1,6 @@
 package utils;
 
-public class UsernameGenerator {
+public abstract class UsernameGenerator {
     public static String usernameGenerator(String firstName, String lastName){
         char[] threeChars = new char[3];
         lastName.getChars(0, 3, threeChars, 0);
